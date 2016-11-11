b93c3999d5c8efe726162a39326eca7000d5fd55
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-11 09:58:09 -0600

    Update main.java

--- main.java
+++ main.java
@@ -26,29 +26,29 @@ public class Main {//card.numArr[0]==ace,2,3,4  card.numArr[1]=spades,hearts,clu
 			return "3 of a Kind";
 		/*2 pair*/}else if((card1.numArr[0]==card2.numArr[0]&&card3.numArr[0]==card4.numArr[0])||(card2.numArr[0]==card3.numArr[0]&&card1.numArr[0]==card4.numArr[0])||(card3.numArr[0]==card4.numArr[0]&&card1.numArr[0]==card2.numArr[0])||(card1.numArr[0]==card3.numArr[0]&&card2.numArr[0]==card4.numArr[0])||(card1.numArr[0]==card4.numArr[0]&&card2.numArr[0]==card3.numArr[0])||(card2.numArr[0]==card4.numArr[0]&&card1.numArr[0]==card3.numArr[0])){
 			return "2 Pair";
-		}else if(card1.numArr[0]==card2.numArr[0]||/*PICK UP HERE*/card1.number2==card3.number2||card1.number2==card4.number2||card2.number2==card3.number2||card2.number2==card4.number2||card3.number2==card4.number2){
+		/*One pair*/}else if(card1.numArr[0]==card2.numArr[0]||card1.numArr[0]==card3.numArr[0]||card1.numArr[0]==card4.numArr[0]||card2.numArr[0]==card3.numArr[0]|card2.numArr[0]==card4.numArr[0]||card3.numArr[0]==card4.numArr[0]){
 			return "One Pair";
 		}
 		return "High Card";
 	}	
 	
 	public static String checkCard5(Card card1,Card card2,Card card3,Card card4,Card card5){
-		/*flush*/if(card1.suit2==card2.suit2&&card2.suit2==card3.suit2&&card3.suit2==card4.suit2&&card4.suit2==card5.suit2){
+		/*flush*/if(card1.numArr[1]==card2.numArr[1]&&card2.numArr[1]==card3.numArr[1]&&card3.numArr[1]==card4.numArr[1]&&card4.numArr[1]==card5.numArr[1]){
 			/*royal flushif(royal flush){
 				return 10;
 	WORKING ON		straight flush}else if(straight flush){
 				return 9;
 			}*/
 			return "Flush6";
-		/*4 of a kind*/}else if((/*1*/card2.number2==card3.number2&&card3.number2==card4.number2&&card4.number2==card5.number2)||(/*2*/card1.number2==card3.number2&&card3.number2==card4.number2&&card4.number2==card5.number2)||(/*3*/card1.number2==card2.number2&&card2.number2==card4.number2&&card4.number2==card5.number2)||(/*4*/card1.number2==card2.number2&&card2.number2==card3.number2&&card3.number2==card5.number2)||(/*5*/card1.number2==card2.number2&&card2.number2==card3.number2&&card3.number2==card4.number2)){
+		/*4 of a kind*/}else if((/*1*/card2.numArr[0]==card3.numArr[0]&&card3.numArr[0]==card4.numArr[0]&&card4.number2==card5.number2)||(/*2*/card1.number2==card3.number2&&card3.number2==card4.number2&&card4.number2==card5.number2)||(/*3*/card1.number2==card2.number2&&card2.number2==card4.number2&&card4.number2==card5.number2)||(/*4*/card1.number2==card2.number2&&card2.number2==card3.number2&&card3.number2==card5.number2)||(/*5*/card1.number2==card2.number2&&card2.number2==card3.number2&&card3.number2==card4.number2)){
 			return "4 of a Kind8";
-		/*full house*/}else if(card1.number2==card2.number2&&card3.number2==card4.number2&&card4.number2==card5.number2){
+		/*full house*/}else if(card1.numArr[0]==card2.number2&&card3.number2==card4.number2&&card4.number2==card5.number2){
 			return "Full House7";
-		}else if(card1.number2==card3.number2&&card2.number2==card4.number2&&card4.number2==card5.number2){
+		}else if(card1.number2==card3.numArr[0]&&card2.number2==card4.number2&&card4.number2==card5.number2){
 			return "Full House7";
-		}else if(card1.number2==card4.number2&&card2.number2==card3.number2&&card2.number2==card5.number2){
+		}else if(card1.number2==card4.numArr[0]&&card2.number2==card3.number2&&card2.number2==card5.number2){
 			return "Full House7";
-		}else if(card1.number2==card5.number2&&card2.number2==card3.number2&&card3.number2==card4.number2){
+		}else if(card1.number2==card5.numArr[0]&&card2.number2==card3.number2&&card3.number2==card4.number2){
 			return "Full House7";
 		}else if(card2.number2==card3.number2&&card1.number2==card4.number2&&card4.number2==card5.number2){
 			return "Full House7";
